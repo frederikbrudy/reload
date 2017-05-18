d2ed1f04b8f3ec20d6e7a3d83ee4017758ee1fbd
fbrudy <fb@fbrudy.net>
2017-05-18 12:16:16 +0100

    gloabl reload object update dependencies

--- src/public/reload.js
+++ src/public/reload.js
@@ -5,15 +5,24 @@
 
 import io from 'socket.io-client';
 
+window.reload = window.reload
+    || window.CL
+    || {
+        afterReconnent: true,
+        host: location.hostname+':3000'
+    };
+
 (function reload() {
-    let socket = io();
+    let socket = io(window.reload.host);
     window.addEventListener('load', function () {
         socket.on('connect', function(){
             // console.log(socket.id);
         });
         socket.on('reconnect', (attemptCount) =>{
-            console.log(`reconnected after ${attemptCount} attempts`);
-            window.location.reload();
+            if(window.reload.afterReconnent) {
+                console.log(`reconnected after ${attemptCount} attempts`);
+                window.location.reload();
+            }
         });
         socket.on('reload', (payload) => {
             console.log(`reloading because of reload command`, payload);
